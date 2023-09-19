b5c03310ee25a7d2ce90c791881c6d7f3a4cda07
Filipe Santana <filipe.t.santana@gmail.com>
2023-09-18 22:47:57 -0300

    ajustando direcionamento para as telas corretas

--- src/app/tela-principal/tela-principal/tela-principal.component.ts
+++ src/app/tela-principal/tela-principal/tela-principal.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tela-principal',
   templateUrl: './tela-principal.component.html',
-  styleUrls: ['./tela-principal.component.scss']
+  styleUrls: ['./tela-principal.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class TelaPrincipalComponent {
-
+  constructor(private router: Router) {}
+  botaoVoltar():void {
+    this.router.navigate(['/tela-login']);
+  }
 }
